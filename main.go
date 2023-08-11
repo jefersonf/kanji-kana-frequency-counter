df97fd23ef1199255d86bafd4ec3e5ee9a4f5725
jefersonf <jefersonnpn@gmail.com>
2023-08-10 22:42:13 -0300

    :wrench:  increse timeout to 10

--- main.go
+++ main.go
@@ -227,7 +227,7 @@ func newKanjiKanaScraper(rootURL string, options ...Option) (*kanjiKanaFrequency
 		hiraganas: make(map[string]int),
 	}
 
-	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
 	defer cancel()
 
 	frequencyCounter.routine(ctx, rootURL, *opts.searchDepth)
